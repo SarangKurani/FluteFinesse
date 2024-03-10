72bebecedbd93b396d7631414775a9a65b12e676
Sarang Kurani <sarang.kurani@gmail.com>
2024-03-10 22:43:02 +0530

    Routes added for application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TunerComponent } from './shell/tuner/tuner.component';
+import { MetronomeComponent } from './shell/metronome/metronome.component';
+import { RecorderComponent } from './shell/recorder/recorder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', title: "Tuner", component: TunerComponent },
+    { path: 'tuner', title: "Tuner", component: TunerComponent },
+    { path: 'metronome', title: "Metronome", component: MetronomeComponent },
+    { path: 'recorder', title: "Recorder", component: RecorderComponent },
+];
