a2556b1505397557a7307cba4c254917af09159f
Sarang Kurani <sarang.kurani@gmail.com>
2024-03-11 22:37:53 +0530

    Initial Audio Player component added, and route and shell nav link created for it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TunerComponent } from './shell/tuner/tuner.component';
 import { MetronomeComponent } from './shell/metronome/metronome.component';
 import { RecorderComponent } from './shell/recorder/recorder.component';
+import { PlayerComponent } from './shell/player/player.component';
 
 export const routes: Routes = [
     { path: '', title: "Tuner", component: TunerComponent },
     { path: 'tuner', title: "Tuner", component: TunerComponent },
     { path: 'metronome', title: "Metronome", component: MetronomeComponent },
     { path: 'recorder', title: "Recorder", component: RecorderComponent },
+    { path: 'player', title: "Player", component: PlayerComponent },
 ];
