f51c0de0c8d9a31e0d858ea54c99d66173f58f85
Sarang Kurani <sarang.kurani@gmail.com>
2024-03-16 05:11:45 +0530

    Added buttons, display, input for Metronome function.

--- src/app/shell/metronome/metronome.component.ts
+++ src/app/shell/metronome/metronome.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-metronome',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, FormsModule],
   templateUrl: './metronome.component.html',
   styleUrl: './metronome.component.scss'
 })
 export class MetronomeComponent {
+stopMetronome() {
+throw new Error('Method not implemented.');
+}
+startMetronome() {
+throw new Error('Method not implemented.');
+}
+  selectedBPM: number = 0;
 
 }
